9504fdcecdd5f23547f13068baae05ac3dbb91db
hecto294 <hectorsana1998@hotmail.com>
2026-04-10 20:56:03 -0500

    fix: correct import paths in route files

--- routes/ficharoutes.js
+++ routes/ficharoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getfichas, createficha, updateficha, deleteficha,getficha } from "../routes/src/controllers/fichas.controller.js";
+import { getfichas, createficha, updateficha, deleteficha,getficha } from "./src/controllers/fichas.controller.js";
 
 const router = Router()
 
